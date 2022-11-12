be4fc97b893445ef597e741cedd5e1424d55f123
Crostz <Crostzard@outlook.com>
2022-11-12 13:22:28 -0500

    Card Shuffle 1

--- GitHub GameOff Game Jam 2022/Assets/1_Scripts/CardSystem/DrawManager.cs
+++ GitHub GameOff Game Jam 2022/Assets/1_Scripts/CardSystem/DrawManager.cs
@@ -15,20 +15,34 @@ public class DrawManager : MonoBehaviour, IPointerDownHandler
 
     public static DrawManager Instance; // Singleton
 
-    public ActionCardSO[] availableCards;
+    public ActionCardSO[] cardList;
 
     public DeckManager manager;
     public GameObject cardPrefab;
 
+    public int deckSize = 240;
+    private int amountPerTurn = 20;
+
+    public int pileAmount = 8;
+    private int currentAmount;
+
     float cooldown = 0.2f;
     float timer;
 
-    [HideInInspector]
-    public List<GameObject> pooledCards = new List<GameObject>();
+    [HideInInspector] public List<GameObject> pooledCards = new List<GameObject>();
+
+    private List<ActionCardSO> cropCards = new List<ActionCardSO>();
+    private List<ActionCardSO> buildCards = new List<ActionCardSO>();
+    private List<ActionCardSO> passiveCards = new List<ActionCardSO>();
+
+    private List<ActionCardSO> shuffledCards = new List<ActionCardSO>();
+    private List<ActionCardSO> piledCards = new List<ActionCardSO>();
 
     public void Start()
     {
         CheckNull();
+
+        ShuffleCards();
 
         if (Instance == null) Instance = this;
         else 
@@ -61,6 +75,13 @@ public class DrawManager : MonoBehaviour, IPointerDownHandler
 
     public void GiveCard(int amount = 1) 
     {
+
+        if(currentAmount <= 0) 
+        {
+            Debug.LogError("Drawing deck is out of cards!");
+            return;
+        }
+
         for (int i = 0; i < amount; i++)
         { 
             GameObject card = pooledCards.Count > 0 ? GetPooledCard() : Instantiate(cardPrefab, manager.transform);
@@ -68,6 +89,8 @@ public class DrawManager : MonoBehaviour, IPointerDownHandler
             AssignCardData(card.transform);
 
             manager.AddCardToList(card.transform.GetComponent<CardScript>());
+
+            currentAmount -= 1;
 
         }
     }
@@ -77,9 +100,9 @@ public class DrawManager : MonoBehaviour, IPointerDownHandler
         CardScript cardScript = card.GetComponent<CardScript>();
         if (cardScript == null) return;
 
-        int randInt = Random.Range(0, availableCards.Length);
+        int randInt = Random.Range(0, shuffledCards.Count);
 
-        cardScript.Card = availableCards[randInt];
+        cardScript.Card = shuffledCards[randInt];
     }
 
     private GameObject GetPooledCard() 
@@ -95,11 +118,77 @@ public class DrawManager : MonoBehaviour, IPointerDownHandler
     
     }
 
+
+    // IMPORTANT: 
+
+    // ADD THIS FUNCTION (ShuffleCards) EVERY TIME A TURN ENDS 
+    /// <summary>
+    /// Adds and shuffles cards appropiately to the deck
+    /// </summary>
+    public void ShuffleCards() 
+    {
+
+        currentAmount += amountPerTurn;
+
+        GetCardTypes();
+
+
+        int x = 24;                 // crop card amount
+        int y = Random.Range(3, 5); // build card amount
+        int z = 6 - y;              // livestock card amount
+
+        for (int i = 0; i < 30; i++)
+        {
+            if (i < x)
+            {
+                // Crop cards
+                int rand = Random.Range(0, cropCards.Count);
+                shuffledCards.Add(cropCards[rand]);
+            }
+            else if (i >= x && i < x + z )
+            {
+                // Livestock cards
+                int rand = Random.Range(0, passiveCards.Count);
+                shuffledCards.Add(passiveCards[rand]);
+            }
+            else
+            {
+                // BuildCards
+                int rand = Random.Range(0, buildCards.Count);
+                shuffledCards.Add(buildCards[rand]);
+            }
+            
+
+        }
+    }
+
+    private void GetCardTypes() 
+    {
+        foreach (ActionCardSO c in cardList)
+        {
+            if (c is BuildingCard)
+            {
+                buildCards.Add(c);
+                continue;
+            }
+            if (c is PassiveCard)
+            {
+                passiveCards.Add(c);
+                continue;
+            }
+            if (c is SeedCard)
+            {
+                cropCards.Add(c);
+                continue;
+            }
+        }
+    }
+
     private void CheckNull() 
     {
         Assert.IsNotNull(cardPrefab, $"{ GetType().Name} missing required editor input 'cardPrefab'");
         Assert.IsNotNull(manager, $"{ GetType().Name} missing required editor input 'manager'");
-        Assert.IsNotNull(availableCards, $"{ GetType().Name} missing required editor input 'availableCards'");
+        Assert.IsNotNull(cardList, $"{ GetType().Name} missing required editor input 'availableCards'");
     }
 
 }
