92383294d38b1b6a3eaeae98aa290a0694e6aee7
ballardi <1449350+ballardi@users.noreply.github.com>
2022-11-13 06:03:44 +1100

    update tile and tileforeground logic to include a huge number of updates

--- GitHub GameOff Game Jam 2022/Assets/Scripts/TileForegrounds/TileForeground.cs
+++ GitHub GameOff Game Jam 2022/Assets/Scripts/TileForegrounds/TileForeground.cs
@@ -8,92 +8,90 @@ using UnityEngine.Assertions;
 public class TileForeground : MonoBehaviour {
 
     [Header("Debug options for testing only")]
-    [Tooltip("When set, the object will auto initialize on Awake and use the state settings already set against the object.")]
-    [SerializeField] private bool _debugMode_AutoInit;
+    [Tooltip("When set, the object will auto initialize on Awake and use the state settings marked here as defaults.")]
+    [SerializeField] private bool          _debugMode_AutoInitUsingDefaults;
+    [SerializeField] private TileType      _defaultTileType;
+    [SerializeField] private BuildingCard  _defaultBuidlingCard;
+    [SerializeField] private LivestockCard _defaultLivestockCard;
+    [SerializeField] private SeedCard      _defaultSeedCard;
+    [SerializeField] private SeasonType    _defaultSeason;
+    [SerializeField] private float         _defaultAgePercentage;
 
-    [Header("State")]
-    [SerializeField] private ForegroundType _foregroundType;
-    [SerializeField] private TileType   _tileType;
-    [SerializeField] private AnimalType _animalType;
-    [SerializeField] private SeedType   _seedType;
-    [SerializeField] private SeasonType _season;
-
-    [Header("Mappings to UI elements")]
-    [SerializeField] private GameObject forestPrefab;
-    [SerializeField] private GameObject cowPrefab;
-    [SerializeField] private GameObject pigPrefab;
-    [SerializeField] private GameObject beesPrefab;
+    [Header("Mappings to foreground UI elements for tiles which are just a base")]
     [SerializeField] private GameObject lakePrefab;
-    [SerializeField] private GameObject forrestPrefab;
-    [SerializeField] private GameObject wheatPrefab;
-    [SerializeField] private GameObject oatsPrefab;
-    [SerializeField] private GameObject flowersPrefab;
+    [SerializeField] private GameObject forestPrefab;
 
     private enum ForegroundType { TILE, ANIMAL, SEED }
     private bool _isInitialized;
     private GameObject _child;
 
     private void Awake() {
-        if (_debugMode_AutoInit) {
-            Initialize(
-                transform.parent.GetComponent<SpriteRenderer>().sortingOrder,
-                true,
-                SeasonType.SUMMER);
+
+        if (_debugMode_AutoInitUsingDefaults) {
+            int tileSortOrder = transform.parent.GetComponent<SpriteRenderer>().sortingOrder;
+            if      (_defaultLivestockCard != null) { Initialize(_defaultLivestockCard, tileSortOrder, true, _defaultSeason); } 
+            else if (_defaultSeedCard      != null) { Initialize(_defaultSeedCard,      tileSortOrder, true, _defaultSeason); } 
+            else if (_defaultBuidlingCard  != null) { Initialize(_defaultBuidlingCard,  tileSortOrder, true, _defaultSeason); } 
+            else {                                    Initialize(_defaultTileType,      tileSortOrder, true, _defaultSeason); }
+            //UpdateForSeasonAndAgePercentage(_defaultSeason, _defaultAgePercentage);
         }
+
     }
 
-    public void Initialize(int tileSortOrder, bool alwaysAnimate, SeasonType season) {
+
+    public void Initialize(TileType tileType, int tileSortOrder, bool alwaysAnimate, SeasonType season) {
+        switch (tileType) {
+            case TileType.LAKE: _child = Instantiate(lakePrefab, transform, false); break;
+            case TileType.FOREST: _child = Instantiate(forestPrefab, transform, false); break;
+            case TileType.MOUNTAIN: break;
+            case TileType.PLAINS: break;
+            case TileType.HILLS: break;
+            default: Debug.LogError($"TileForeground {this.name} never expected _tileType of {tileType}?"); break;
+        }
+        ForegroundIndependentInit(tileSortOrder, alwaysAnimate, season);
+    }
+    public void Initialize(BuildingCard buildingCard, int tileSortOrder, bool alwaysAnimate, SeasonType season) {
+        if (buildingCard.tileForegroundChildPrefab == null)
+            return;
+        _child = Instantiate(buildingCard.tileForegroundChildPrefab, transform, false); 
+        ForegroundIndependentInit(tileSortOrder, alwaysAnimate, season);
+    }
+    public void Initialize(LivestockCard livestockCard, int tileSortOrder, bool alwaysAnimate, SeasonType season) {
+        if (livestockCard.tileForegroundChildPrefab == null)
+            return;
+        _child = Instantiate(livestockCard.tileForegroundChildPrefab, transform, false);
+        ForegroundIndependentInit(tileSortOrder, alwaysAnimate, season);
+    }
+    public void Initialize(SeedCard seedCard, int tileSortOrder, bool alwaysAnimate, SeasonType season) {
+        if (seedCard.tileForegroundChildPrefab == null)
+            return;
+        _child = Instantiate(seedCard.tileForegroundChildPrefab, transform, false);
+        ForegroundIndependentInit(tileSortOrder, alwaysAnimate, season);
+    }
+
+    private void ForegroundIndependentInit(int tileSortOrder, bool alwaysAnimate, SeasonType season) {
         Assert.IsFalse(_isInitialized, $"TileForeground {this.name} never expected to be initialized twice. Check that debug flag is off? Check Initialize() isn't called twice somehow?");
-
-        // isntantiate the right prefab under the foreground object based on the initialize params
-        if (!_debugMode_AutoInit) {
-            // set _tileType based on inputs
-            // set _animalType based on inputs
-            // set _seedType based on inputs
-            _season = season;
-        }
-
-        // instantiate the child prefab
-        switch (_foregroundType) {
-            case ForegroundType.TILE:
-                switch (_tileType) {
-                    case BuildingManagement.TileType.LAKE: _child = Instantiate(lakePrefab, transform, false); break;
-                    case BuildingManagement.TileType.FOREST: _child = Instantiate(forrestPrefab, transform, false); break;
-                    case BuildingManagement.TileType.MOUNTAIN: break;
-                    case BuildingManagement.TileType.PLAINS: break;
-                    case BuildingManagement.TileType.HILLS: break;
-                    default: Debug.LogError($"TileForeground {this.name} never expected _tileType of {_tileType}?"); break;
-                }
-                break;
-            case ForegroundType.ANIMAL:
-                switch (_animalType) {
-                    case BuildingManagement.AnimalType.COW: _child = Instantiate(cowPrefab, transform, false); break;
-                    case BuildingManagement.AnimalType.PIG: _child = Instantiate(pigPrefab, transform, false); break;
-                    case BuildingManagement.AnimalType.BEES: _child = Instantiate(beesPrefab, transform, false); break;
-                    default: Debug.LogError($"TileForeground {this.name} never expected _animalType of {_animalType}?"); break;
-                }
-                break;
-            case ForegroundType.SEED:
-                switch (_seedType) {
-                    case BuildingManagement.SeedType.WHEAT: _child = Instantiate(wheatPrefab, transform, false); break;
-                    case BuildingManagement.SeedType.OATS: _child = Instantiate(oatsPrefab, transform, false); break;
-                    case BuildingManagement.SeedType.FLOWERS: _child = Instantiate(flowersPrefab, transform, false); break;
-                    default: Debug.LogError($"TileForeground {this.name} never expected _seedType of {_seedType}?"); break;
-                }
-                break;
-            default: Debug.LogError($"TileForeground {this.name} never expected _foregroundType of {_foregroundType}?"); break;
-        }
-
-        // initialize the prefab
-        if (_child != null) {
-            _child.GetComponent<TileForegroundChild>().PositionSpritesBasedOnTileSortOrder(tileSortOrder);
-            _child.GetComponent<TileForegroundChild>().UpdateSpritesBasedOnParams(_season);
-        }
-
+        _child.GetComponent<TileForegroundChild>().PositionSpritesBasedOnTileSortOrder(tileSortOrder);
+        _child.GetComponent<TileForegroundChild>().UpdateForSeasonAndAgePercentage(season, 0);
         _isInitialized = true;
     }
 
-    public void UpdateBasedOnParams(SeasonType season) {
-        _child.GetComponent<TileForegroundChild>().UpdateSpritesBasedOnParams(_season);
+    /// <summary>
+    /// to be called during computer phase
+    /// </summary>
+    /// <param name="season"></param>
+    /// <param name="agePercentage"></param>
+    public void UpdateForSeasonAndAgePercentage(SeasonType season, float agePercentage) {
+        _child.GetComponent<TileForegroundChild>().UpdateForSeasonAndAgePercentage(season, agePercentage);
     }
+    public void StartAnimating() {
+        _child.GetComponent<TileForegroundChild>().StartAnimating();
+    }
+    public void StopAnimating() {
+        _child.GetComponent<TileForegroundChild>().StopAnimating();
+    }
+
+
+
+
 }
