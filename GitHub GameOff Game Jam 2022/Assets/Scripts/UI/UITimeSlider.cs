f9ccd3d3344ec055af63d4542b123f64a0e67847
GraySfx <music@graydonsamuelson.com>
2022-11-14 16:46:31 -0500

    UI Time Manager SFX Implementation
    Implemented SFX into Unity for the Time Manager UI

--- GitHub GameOff Game Jam 2022/Assets/Scripts/UI/UITimeSlider.cs
+++ GitHub GameOff Game Jam 2022/Assets/Scripts/UI/UITimeSlider.cs
@@ -1,14 +1,23 @@
 namespace TimeManagement
 {
+    using FMODUnity;
     using UnityEngine;
 
     public class UITimeSlider : InspectorReferenceChecker
     {
         [SerializeField] private TimePanel timePanel;
 
+        [Header("Slider Sounds")]
+        [SerializeField] private EventReference turnProgressMeterEventName;
+        [SerializeField] private EventReference endTurnNotificationChimeEventName;
+
         public void TriggerNewSeason() => timePanel.TriggerNewSeasonAnimation();
 
         public void UpdateTimeTextComponents() => timePanel.UpdateTextComponents();
+
+        // Sound Methods
+        public void PlayTurnProgressMeterSound() => RuntimeManager.PlayOneShot(turnProgressMeterEventName);
+        public void PlayEndTurnNotificationChimeSound() => RuntimeManager.PlayOneShot(endTurnNotificationChimeEventName);
 
         protected override object[] CheckForMissingReferences() => new object[] { timePanel };
 
