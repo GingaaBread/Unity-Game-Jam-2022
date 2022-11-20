df5b8dd2a23c7e5b87bf2eee0a42838054ed0e4c
ballardi <1449350+ballardi@users.noreply.github.com>
2022-11-21 03:06:29 +1100

    winter and autumn animations

--- GitHub GameOff Game Jam 2022/Assets/Scripts/SeasonForegrounds/SeasonForegroundScript.cs
+++ GitHub GameOff Game Jam 2022/Assets/Scripts/SeasonForegrounds/SeasonForegroundScript.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class SeasonForegroundScript : MonoBehaviour
 {
-
+    [Header("animation Offset")]
     public float CycleOffset;
+    public bool RandomizeOffset;
 
-    void OnEnable()
-    {
-        this.GetComponent<Animator>().SetFloat("CycleOffset", CycleOffset);
+    [Header("Parent position")]
+    public float xPosOffsetMinBound;
+    public float xPosOffsetMaxBound;
+    public float yPosOffsetMinBound;
+    public float yPosOffsetMaxBound;
+
+
+    void OnEnable() {
+
+        Random.InitState(gameObject.GetInstanceID());
+
+        if (RandomizeOffset) {
+            this.GetComponent<Animator>().SetFloat("CycleOffset", Random.Range(0f,1f));
+        } else {
+            this.GetComponent<Animator>().SetFloat("CycleOffset", CycleOffset);
+        }
+
+        float parentXPos = Random.Range(xPosOffsetMinBound, xPosOffsetMaxBound);
+        float parentYPos = Random.Range(yPosOffsetMinBound, yPosOffsetMaxBound);
+        transform.parent.localPosition = new Vector3(parentXPos, parentYPos, transform.parent.localPosition.z);
+
     }
 
 }
