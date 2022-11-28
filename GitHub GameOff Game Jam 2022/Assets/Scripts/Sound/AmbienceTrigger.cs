7b0cd09001142ac9f6a3160ba7a5f6b04144bbbe
= <=>
2022-11-28 01:21:50 +0100

    Music order

--- GitHub GameOff Game Jam 2022/Assets/Scripts/Sound/AmbienceTrigger.cs
+++ GitHub GameOff Game Jam 2022/Assets/Scripts/Sound/AmbienceTrigger.cs
@@ -1,18 +1,42 @@
+using FMOD.Studio;
 using FMODUnity;
+using TimeManagement;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class AmbienceTrigger : MonoBehaviour
+public class AmbienceTrigger : ComputerPhaseStep
 {
     [SerializeField] private EventReference farmAmbience;
+    [SerializeField] private EventReference music;
 
     public static AmbienceTrigger Instance;
+
+    private EventInstance musicEventInstance;
 
     private void Start()
     {
         Assert.IsNull(Instance, "AmbienceTrigger instance already exists!");
         Instance = this;
+
+        musicEventInstance = RuntimeManager.CreateInstance(music);
+        musicEventInstance.start();
     }
 
     public void PlayFarmAmbience() => RuntimeManager.PlayOneShot(farmAmbience);
+
+    public override void StartProcessingForComputerPhase(bool isComputerPhaseDuringGameInit)
+    {
+        if (!isComputerPhaseDuringGameInit && TimeManager.Instance.CurrentTime.IsFirstRoundOfSeason())
+        {
+            // The seasons should be 0 for spring, 1 for summer, 2 for autumn, 3 for winter (and 4 for the will)
+            musicEventInstance.setParameterByName("Season", (int) TimeManager.Instance.CurrentTime.SeasonInYear);
+        }
+
+        OnFinishProcessing.Invoke();
+    }
+
+    protected override object[] CheckForMissingReferences()
+    {
+        return new object[0];
+    }
 }
