1da706a9549e6cfd480d14aba70822a75597a483
Rohaid <rohaidb1999@gmail.com>
2022-11-30 14:50:23 -0500

    Added new UI elements + Edited Will Font + Edited Lose/Win text

--- GitHub GameOff Game Jam 2022/Assets/Scripts/UI/Tutorial/TutorialSO.cs
+++ GitHub GameOff Game Jam 2022/Assets/Scripts/UI/Tutorial/TutorialSO.cs
@@ -7,8 +7,6 @@ public class TutorialSO : ScriptableObject{
 
     public Sprite[] speakers;
     [TextArea(6,6)]
-    //added because Unity 2021 LTS problem with showing index 0 TextArea arrays :/
-    [NonReorderable]
     public string[] lines;
     public Sprite[] screenshots;
     
